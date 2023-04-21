3cefd37bdefb423d15188ce6471a14e0fa3a02c1
Kellerberg <r.carter.hpl@gmail.com>
2023-04-22 02:25:08 +0300

    Added Allure report.

--- test_main.py
+++ test_main.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 import random
 import time
@@ -8,122 +9,159 @@ from selene import be, have
 @pytest.fixture
 def set_browser_configuration():
     browser.config.browser_name = 'firefox'
-    browser.open('https://www.europeana.eu/en')
+    browser.open('about:blank')
     browser.driver.set_window_size(width=1400, height=800)
     browser.config.hold_browser_open = False
 
 
-@pytest.fixture
-def entering_advanced_search_form(set_browser_configuration):
-    browser.element('.form-control').should(be.visible).click()
-    browser.element('#search-form-options > a').should(be.visible).click()
+@allure.step("Open main page.")
+def open_main_page():
+    browser.open('https://www.europeana.eu/en')
 
 
-def test_searching_from_main(set_browser_configuration):
-    search_input = 'cats'
-    browser.element('.form-control').should(be.visible).click().type(search_input).press_enter()
-    browser.element('.col-results').should(have.no.text('0 results'))
-    browser.element('.col-results').should(have.text(search_input))
+@allure.step("Entering search request.")
+def enter_search_request(search):
+    browser.element('[data-qa="search box"]').should(be.visible).click().type(search).press_enter()
 
 
-def test_searching_search_for_everything(set_browser_configuration):
-    browser.element('#show-search-button').click()
-    browser.element('#search-form-options > a').should(be.visible).click()
-    browser.element('.col-results').should(have.text('results'))
+@allure.step("Checking search results.")
+def checking_search_results(search):
+    browser.element('[data-qa="search page"]').should(have.text(search))
+
+@allure.step("Show search button.")
+def show_search_button():
+    browser.element('[data-qa="show search button"]').click()
+
+@allure.step("Searching for everything.")
+def searching_for_everything():
+    show_search_button()
+    browser.element('[data-qa="search entire collection button"]').should(be.visible).click()
+
+@allure.step("Quick search.")
+def quick_search(search):
+    open_main_page()
+    show_search_button()
+    enter_search_request(search)
+
+def test_searching_from_main(set_browser_configuration, search='cats'):
+    quick_search(search)
+    checking_search_results(search)
 
 
-def test_searching_by_input(set_browser_configuration):
-    time.sleep(1)
-    search_input = 'cats'
-    browser.element('#show-search-button').click()
-    browser.element('.form-control').should(be.visible).type(search_input).press_enter()
-    browser.element('.col-results').should(have.no.text('0 results'))
-    browser.element('.col-results').should(have.text(search_input))
+def test_searching_search_for_everything(set_browser_configuration, search='results'):
+    open_main_page()
+    searching_for_everything()
+    checking_search_results(search)
+
+
+def test_searching_by_input(set_browser_configuration, search='dogs'):
+    quick_search(search)
+    checking_search_results(search)
+    browser.element('[data-qa="search page"]').should(have.no.text('0 results'))
 
 
 def test_searching_by_input_0_results(set_browser_configuration):
-    time.sleep(1)
     random.seed()
     random_request = ''
     for x in range(15):
         random_request = random_request + random.choice(list('12345678909qwertyuiopasdfghjklzxcvbnm'))
 
-    search_input = random_request
-    browser.element('#show-search-button').click()
-    browser.element('.form-control').should(be.visible).type(search_input).press_enter()
-    browser.element('.col-results').should(have.text('0 results'))
+    search = random_request
+    open_main_page()
+    show_search_button()
+    enter_search_request(search)
+    search = '0 results'
+    checking_search_results(search)
 
 
-def test_checking_advanced_search_opens(set_browser_configuration, entering_advanced_search_form):
-    browser.element('.filters-title').should(have.text('Filter results'))
+def test_checking_advanced_search_opens(set_browser_configuration):
+    open_main_page()
+    searching_for_everything()
+    browser.element('[data-qa="side filters"]').should(have.text('Filter results'))
 
 
-def test_applying_search_filters(set_browser_configuration, entering_advanced_search_form):
+def test_applying_search_filters(set_browser_configuration):
+    open_main_page()
+    searching_for_everything()
     time.sleep(2)
-    browser.element('[data-qa="collection side facet dropdown button"]').click()
-    browser.element('[data-qa="art collection field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Art'))
+    with allure.step("Picking THEME."):
+        browser.element('[data-qa="collection side facet dropdown button"]').click()
+        browser.element('[data-qa="art collection field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Art'))
 
-    browser.element('[data-qa="TYPE side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="IMAGE TYPE field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Image'))
+    with allure.step("Picking TYPE."):
+        browser.element('[data-qa="TYPE side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="IMAGE TYPE field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Image'))
 
-    browser.element('[data-qa="REUSABILITY side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="open REUSABILITY field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Yes'))
+    with allure.step("Picking REUSABILITY."):
+        browser.element('[data-qa="REUSABILITY side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="open REUSABILITY field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Yes'))
 
-    browser.element('[data-qa="COUNTRY side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="Netherlands COUNTRY field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Netherlands'))
+    with allure.step("Picking COUNTRY."):
+        browser.element('[data-qa="COUNTRY side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="Netherlands COUNTRY field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Netherlands'))
 
-    browser.element('[data-qa="LANGUAGE side facet dropdown button"]').click()
-    browser.element('[data-qa="en LANGUAGE field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('English'))
+    with allure.step("Picking LENGUAGE."):
+        browser.element('[data-qa="LANGUAGE side facet dropdown button"]').click()
+        browser.element('[data-qa="en LANGUAGE field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('English'))
 
-    browser.element('[data-qa="PROVIDER side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="The European Library PROVIDER field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('The European Library'))
+    with allure.step("Picking PROVIDER."):
+        browser.element('[data-qa="PROVIDER side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="The European Library PROVIDER field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('The European Library'))
 
-    browser.element('[data-qa="DATA_PROVIDER side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="National Library of the Netherlands DATA_PROVIDER field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('National Library of the Netherlands'))
+    with allure.step("Picking DATA_PROVIDER."):
+        browser.element('[data-qa="DATA_PROVIDER side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="National Library of the Netherlands DATA_PROVIDER field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('National Library of the Netherlands'))
 
-    browser.element('[data-qa="COLOURPALETTE side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="#000000 COLOURPALETTE field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Black'))
+    with allure.step("Picking COLOURPALETTE."):
+        browser.element('[data-qa="COLOURPALETTE side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="#000000 COLOURPALETTE field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Black'))
 
-    browser.element('[data-qa="IMAGE_ASPECTRATIO side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="landscape IMAGE_ASPECTRATIO field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Landscape'))
+    with allure.step("Picking IMAGE_ASPECTRATIO."):
+        browser.element('[data-qa="IMAGE_ASPECTRATIO side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="landscape IMAGE_ASPECTRATIO field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Landscape'))
 
-    browser.element('[data-qa="IMAGE_SIZE side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="medium IMAGE_SIZE field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Medium 0.5-1MP (e.g. 850x850px)'))
+    with allure.step("Picking IMAGE_SIZE."):
+        browser.element('[data-qa="IMAGE_SIZE side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="medium IMAGE_SIZE field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Medium 0.5-1MP (e.g. 850x850px)'))
 
-    browser.element('[data-qa="MIME_TYPE side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="image/jpeg MIME_TYPE field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('JPEG'))
+    with allure.step("Picking MIME_TYPE."):
+        browser.element('[data-qa="MIME_TYPE side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="image/jpeg MIME_TYPE field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('JPEG'))
 
-    browser.element('[data-qa="RIGHTS side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="*/publicdomain/mark/* RIGHTS field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Public Domain Mark'))
+    with allure.step("Picking RIGHTS."):
+        browser.element('[data-qa="RIGHTS side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="*/publicdomain/mark/* RIGHTS field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Public Domain Mark'))
 
-    browser.element('[data-qa="contentTier side facet dropdown button"]').should(be.clickable).click()
-    browser.element('[data-qa="2 contentTier field"]').should(be.clickable).click()
-    time.sleep(0.5)
-    browser.element('[data-qa="side filters"]').should(have.text('Medium quality'))
+    with allure.step("Strange...."):
+        browser.element('[data-qa="contentTier side facet dropdown button"]').should(be.clickable).click()
+        browser.element('[data-qa="2 contentTier field"]').should(be.clickable).click()
+        time.sleep(0.5)
+        browser.element('[data-qa="side filters"]').should(have.text('Medium quality'))
 
-    browser.element('[data-qa="reset filters button"]').should(be.visible).click()
-    browser.element('[data-qa="reset filters button"]').should(be.not_.visible)
+    with allure.step("Testing RESET button."):
+        browser.element('[data-qa="reset filters button"]').should(be.visible).click()
+        browser.element('[data-qa="reset filters button"]').should(be.not_.visible)
